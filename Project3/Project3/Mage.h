a1920292b96bad3aa6d2ae2ad53b7feedf883f49
Trevis Jontaé Fields <tfields3@student.rcc.edu>
2017-04-20 04:56:27 -0700

    mage.h added

---
+++ Project3/Project3/Mage.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+
+class Mage
+{
+private:
+  std::string _name;
+  
+public:
+  Mage(std::string name);
+  ~Mage();
+  
+  std::string GetName()
+  const {return _name;}
+  std::string Attack()
+  {return _name + " casts magic missles at the darkness!";}
+};
