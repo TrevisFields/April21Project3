2b955dc8f4781b834a52a93f4897a7a5b324a4b7
Trevis Jontaé Fields <tfields3@student.rcc.edu>
2017-04-20 05:10:39 -0700

    warrior.h added

---
+++ Project3/Project3/Warrior.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+
+class Warrior
+{
+private:
+  std::string _name;
+  
+public:
+  Warrior(std::string name);
+  ~Warrior();
+  
+  std::string GetName()
+  const {return _name;}
+  std::string Attack()
+  {return _name + " shouts ' DO YOU LIKE MY SWORD SWORD SWORD.\n MY DIAMOND SWORD!";}
+};
