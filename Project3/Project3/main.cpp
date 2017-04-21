0576b3c9b3ed374e7881a99a3b0dcd3a1f024126
Trevis Jontaé Fields <tfields3@student.rcc.edu>
2017-04-20 18:16:27 -0700

    comments added

--- Project3/Project3/main.cpp
+++ Project3/Project3/main.cpp
@@ -51,3 +51,5 @@ void WelcomeMessage()
 {
   cout << "Hello and welcome to the program code for File/Folder search.\n";
 }
+
+  //messed up the code to much and cannot find all of my errors. Gave up on the code for the time being, to much to look through to figure out what is wrong with the code. Going to go up to MVC Friday to see if i can get some more help with the code. Hopefully I can work through all of the problems i created for myself.
